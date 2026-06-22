a4d908a8d0bc97223bdbf5744ce15ed19eeaf57d
Rachel Sherard <rsherard@mac.com>
2026-06-22 06:47:15 -0400

    implement structural basics

---
+++ src/content/consts.ts
@@ -0,0 +1,11 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = `RamoSites`;
+export const SITE_DESCRIPTION = `Build a home that's Yours`;
+
+export const LINKS = [
+    { name: 'Home', href: '/' },
+    { name: 'Survey', href: '/' },
+    { name: 'Email', href: '/' }
+];

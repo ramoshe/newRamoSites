6b00be723a719a4d90777e9464a4f25cb56339a0
Rachel Sherard <rsherard@mac.com>
2026-06-22 22:58:34 -0400

    implement color scheme and finish out basic styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,27 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-	fonts: [
-		{
-			provider: fontProviders.google(),
-			name: "Nunito",
-			cssVariable: "--font-nunito",
-		},
+  fonts: [
+      {
+          provider: fontProviders.google(),
+          name: "Roboto Mono",
+          cssVariable: "--font-base",
+      },
+      {
+          provider: fontProviders.google(),
+          name: "Coiny",
+          cssVariable: "--font-fancy",
+      },
+      {
+          provider: fontProviders.google(),
+          name: "Shadows Into Light Two",
+          cssVariable: "--font-note",
+      },
 	],
+
+  integrations: [icon()],
 });

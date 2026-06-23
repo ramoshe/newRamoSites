1c533bcf139c3ffb9f88f2c6c98061166d29673d
Rachel Sherard <rsherard@mac.com>
2026-06-23 08:51:51 -0400

    add email form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,25 @@
-// @ts-check
 import { defineConfig, fontProviders } from "astro/config";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  fonts: [
-      {
-          provider: fontProviders.google(),
-          name: "Roboto Mono",
-          cssVariable: "--font-base",
-      },
-      {
-          provider: fontProviders.google(),
-          name: "Coiny",
-          cssVariable: "--font-fancy",
-      },
-      {
-          provider: fontProviders.google(),
-          name: "Shadows Into Light Two",
-          cssVariable: "--font-note",
-      },
+	fonts: [
+		{
+			provider: fontProviders.google(),
+			name: "Roboto Mono",
+			cssVariable: "--font-base",
+		},
+		{
+			provider: fontProviders.google(),
+			name: "Coiny",
+			cssVariable: "--font-fancy",
+		},
+		{
+			provider: fontProviders.google(),
+			name: "Shadows Into Light Two",
+			cssVariable: "--font-note",
+		},
 	],
 
-  integrations: [icon()],
+	integrations: [icon()],
 });
